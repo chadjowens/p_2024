faac8be30dc2ad300dc7cc35315be872897a9598
chadjowens <chadjowens@gmail.com>
2024-03-27 17:33:21 -0500

    Added intro component with profile image/txt/animations.
    Updated next.config to allow served images.
    
    Updated eslint config to handle apostrophes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 // import Image from 'next/image'
+import Intro from '@/components/intro';
+
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-
-
-
+      <Intro />
 
     </main>
   );
