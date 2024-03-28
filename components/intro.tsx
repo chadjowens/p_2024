bb26ca135b03d5ed208fc334c3a8426644f90bce
chadjowens <chadjowens@gmail.com>
2024-03-28 18:09:34 -0500

    Updated to include ActiveSectionContextProvider to track nav selection and scroll.
    Imported active-section-context to section components.
    
    Included code for useInView from react-intersection-observer.

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useActiveSectionContext } from '@/context/active-section-context';
+import { useInView } from 'react-intersection-observer';
 
 export default function intro() {
+  const { ref, inView } = useInView({
+    threshold: 0.50,
+  });
+
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Home');
+    }
+  }, [inView, setActiveSection, timeOfLastClick]);
   
     return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
         <div className="flex items-center justify-center">
             <div className='relative'>
                 <motion.div  
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,15 +1,27 @@
-// "use client";
+"use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import SectionHeading from './section-heading';
 import { projectsData } from '@/lib/data';
-// import Image from 'next/image';
-// import { useScroll } from 'framer-motion';
 import Project from './project';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Projects() {
+  const { ref, inView } = useInView({
+    threshold: 0.50,
+  });
+
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Projects');
+    }
+  }, [inView, setActiveSection, timeOfLastClick]);
+
   return (
-    <section>
+    <section ref={ref} id="projects" className="scroll-mt-28">
         <SectionHeading>My Projects</SectionHeading>
         <div>
             {projectsData.map((project, index) => (
