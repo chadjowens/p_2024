1b48b7cd134aa98283166a77a91572c7aab369f3
chadjowens <chadjowens@gmail.com>
2024-03-28 12:33:54 -0500

    - Added section divider. - Created About Me section. - Updated to create a reusable section heading.

--- components/intro.tsx
+++ components/intro.tsx
@@ -82,7 +82,7 @@ export default function intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
           href="/CV.pdf"
           download
         >
