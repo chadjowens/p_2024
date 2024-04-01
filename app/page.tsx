87d31faf6f4080fa93d620e218daa5589913e86e
chadjowens <chadjowens@gmail.com>
2024-04-01 12:05:21 -0500

    Updated to add a small footer component.

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Projects from '@/components/projects';
 import Skills from '@/components/skills';
 import Experience from '@/components/experience';
 import Contact from '@/components/contact';
+// import Footer from '@/components/footer';
 
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
+      {/* <Footer /> */}
 
     </main>
   );
