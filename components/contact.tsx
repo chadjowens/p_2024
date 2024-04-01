a4d38a9315f235d275b6f35db4d8c840ceebbe71
chadjowens <chadjowens@gmail.com>
2024-04-01 17:32:23 -0500

    Updated styles to work with dark mode.
    Created theme-switch.tsx and theme-context.tsx.
    
    Updated experience.tsx to troubleshoot styling issues with vertical timeline.
    
    Updated global.css to troubleshoot text input and textarea dark.bg-opacity issues.

--- components/contact.tsx
+++ components/contact.tsx
@@ -27,10 +27,17 @@ export default function Contact() {
 
         >
             <SectionHeading>Contact Me</SectionHeading>
-                <p className="text-gray-700 -mt-6"> Please contact me me at <a className="underline" href="mailto:chadjowens@gmail.com"> chadjowens@gmail.com </a>{" "} or through this form. </p>
+                <p className="text-gray-700 -mt-6 dark:text-white/80"> Please contact me me at <a className="underline" href="mailto:chadjowens@gmail.com"> chadjowens@gmail.com </a>{" "} or through this form. </p>
+                
                 <form 
-                    className="mt-10 flex flex-col"
-                    action={async (formData) => {
+                    className="mt-10 flex flex-col dark:text-black/80 dark:background-color-gray-950 bg-opacity-20 dark:focus:bg-opacity-50"
+                    onSubmit={async (event) => {
+                        // Prevent the form from being submitted
+                        event.preventDefault();
+
+                        // Get the form data
+                        const formData = new FormData(event.target as HTMLFormElement);
+
                         const { data, error } = await sendEmail(formData);
 
                         if (error) {
@@ -41,8 +48,21 @@ export default function Contact() {
                         toast.success("Email sent successfully!");
                     }}
                 >
+                {/* <form 
+                    className="mt-10 flex flex-col dark:text-black/80"
+                    action={async (formData) => {
+                        const { data, error } = await sendEmail(formData);
+
+                        if (error) {
+                            toast.error(error);
+                            return;
+                        }
+
+                        toast.success("Email sent successfully!");
+                    }}
+                > */}
                     <input 
-                    className="h-14 px-4 rounded-lg borderBlack focus:outline-none focus:border-gray-950 focus:border-2" 
+                    className="h-14 px-4 rounded-lg borderBlack focus:outline-none focus:border-gray-950 focus:border-2  transition-all" 
                     name="senderEmail"
                     placeholder="Your email" 
                     type="email" 
@@ -50,7 +70,7 @@ export default function Contact() {
                     maxLength={500}
                 />
                     <textarea 
-                    className="h-52 my-3 p-4 rounded-lg borderBlack focus:outline-none focus:border-gray-950 focus:border-2" 
+                    className="h-52 my-3 p-4 rounded-lg borderBlack focus:outline-none focus:border-gray-950 focus:border-2 transition-all" 
                     name="message"
                     placeholder="Your email"
                     required
--- components/experience.tsx
+++ components/experience.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from 'react'
+import { useSectionInView } from '@/lib/hooks';
 import SectionHeading from './section-heading'
 import {
     VerticalTimeline,
@@ -8,72 +9,194 @@ import {
   } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from '@/lib/data';
-import { useSectionInView } from '@/lib/hooks';
-// import { inView } from 'framer-motion';
-// import { inView } from 'framer-motion';
-
-
-// trying this
 import { useInView } from "react-intersection-observer";
-
+import { useTheme } from '@/context/theme-context';
+// import { inView } from 'framer-motion';
+// import { inView } from 'framer-motion';
 
 // export default function Experience() {
 //     const { ref, inView } = useInView({ 
 //         triggerOnce: true,
 //     });
 
+type ExperienceElementProps = {
+    theme: string
+    item: {
+        date: string
+        icon: React.ReactNode
+        title: string
+        location: string
+        description: string
+    }
+}
+
+const ExperienceElement = ({ theme, item }: ExperienceElementProps) => {
+    const { ref, inView } = useInView({ threshold: 0 })
+    return (
+        <div ref={ref} className='vertical-timeline-element'>
+            <VerticalTimelineElement
+                visible={inView}
+                contentStyle={{
+                    background:
+                        theme === 'light'
+                            ? '#f3f4f6'
+                            : 'rgba(255, 255, 255, 0.05)',
+                    boxShadow: 'none',
+                    border: 
+                        theme === 'light'
+                            ? '0.1rem solid #9ca3af'
+                            : '0.1rem solid rgba(255, 255, 255, 0.25)',
+                    textAlign: 'left',
+                    padding: '1.3rem 2rem',
+                }}
+                contentArrowStyle={{
+                    borderRight:
+                        theme === 'light'
+                            ? '0.4rem solid #9ca3af'
+                            : '0.4rem solid rgba(255, 255, 255, 0.5)',
+                }}
+                date={item.date}
+                icon={item.icon}
+                iconStyle={{
+                    background:
+                        theme === 'light' ? 'white' : 'rgba(29, 36, 50, 0.95)',
+                    fontSize: '1.5rem',
+                }}
+            >
+                <h3 className='font-semibold capitalize'>{item.title}</h3>
+                <p className='font-normal !mt-0'>{item.location}</p>
+                <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>
+                    {item.description}
+                </p>
+            </VerticalTimelineElement>
+        </div>
+    )
+}
+
 export default function Experience() {
     const { ref } = useSectionInView('Experience');
-    // const { theme } = useTheme();
-    console.log('Rendering Experience');
-
+    const { theme } = useTheme()
     return (
-        <section ref={ref} id="experience" className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'>
+        <section
+            ref={ref}
+            id='experience'
+            className='scroll-mt-28 mb-28 sm:mb-40'
+        >
             <SectionHeading>My Experience</SectionHeading>
-            <VerticalTimeline lineColor="">
-                {experiencesData.map((item, index) => {
-                    // const { ref, inView } = useInView({
-                    //     // triggerOnce: true,
-                    // });
-                    const { ref, inView } = useInView({ threshold: 0 });
-                    return (
-                        <div key={index} ref={ref} className="vertical-timeline-element">
-                            {/* // <React.Fragment key={index}> */}
-                            <VerticalTimelineElement 
-                            visible={inView}
-                            // animate={true}
-                            contentStyle={{
-                                background: "#f3f4f6",
-                                //   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                                boxShadow: "none",
-                                border: "1px solid rgba(0, 0, 0, 0.05)",
-                                textAlign: "left",
-                                padding: "1.3rem 2rem",
-                            }}
-                            contentArrowStyle={{
-                                borderRight: "0.4rem solid #9ca3af",
-                                //   theme === "light"
-                                //     ? "0.4rem solid #9ca3af"
-                                //     : "0.4rem solid rgba(255, 255, 255, 0.5)",
-                            }}
-                            date={item.date}
-                            icon={item.icon}
-                            iconStyle={{
-                                background: "white",
-                                //   theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                                fontSize: "1.5rem",
-                            }}
-
-                            >
-                                <h3 className="font-semibold capitalize">{item.title}</h3>
-                                <p className="font-normal !mt-0">{item.location}</p>
-                                <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
-                            </VerticalTimelineElement>
-                            {/* // </React.Fragment> */}
-                        </div>
-                        );
-                    })}
+            <VerticalTimeline lineColor=''>
+                {experiencesData.map((item, index) => (
+                    <ExperienceElement key={index} theme={theme} item={item} />
+                ))}
             </VerticalTimeline>
         </section>
     )
 }
+
+//     return (
+//         <section 
+//         id="experience" 
+//         ref={ref} 
+//         className="smb-28 max-w-[45rem] scroll-mt-28 text-center leading-8 sm:mb-40"
+//         >
+//             <SectionHeading>My experience</SectionHeading>
+//             <VerticalTimeline lineColor="">
+//                 {experiencesData.map((item, index) => {
+//                     const { ref, inView } = useInView({ 
+//                         triggerOnce: true,
+//                     });
+//                     let { theme } = useTheme();
+//                     return (
+//                         <div key={index} ref={ref} className="vertical-timeline-element">
+//                             <VerticalTimelineElement
+//                                 contentStyle={{
+//                                     // background: '#f3f4f6',
+//                                     boxShadow: "none",
+//                                     // theme = "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+//                                     theme,
+//                                     border: "1px solid rgba(0, 0, 0, 0.05)",
+//                                     textAlign: "left",
+//                                     padding: "1.3rem 2rem",
+//                                 }}
+
+//                                 contentArrowStyle={{
+//                                     borderRight: '0.4rem solid #9ca3af',
+//                                     // theme === "light"
+//                                     //     ? "0.4rem solid #9ca3af"
+//                                     //     : "0.4rem solid rgba(255, 255, 255, 0.5)",
+//                                 }}
+//                                 visible={inView}
+//                                 date={item.date}
+//                                 icon={item.icon}
+//                                 iconStyle={{
+//                                     background: 'white',
+//                                     // theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+//                                     fontSize: "1.5rem",
+//                                 }}
+//                                 >
+//                                 <h3 className="font-semibold capitalize">{item.title}</h3>
+//                                 <p className="font-normal !mt-0">{item.location}</p>
+//                                 <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+//                                     {item.description}
+//                                 </p>
+//                             </VerticalTimelineElement>
+//                         </div>
+//                     );
+//                 })}
+//             </ VerticalTimeline>
+//         </section>
+//     );
+// }
+
+
+// return (
+//     <section ref={ref} id="experience" className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'>
+//         <SectionHeading>My Experience</SectionHeading>
+//         <VerticalTimeline lineColor="">
+//             {experiencesData.map((item, index) => {
+//                 // const { ref, inView } = useInView({
+//                 //     // triggerOnce: true,
+//                 // });
+// 
+//                 const { ref, inView } = useInView({ threshold: 0 });
+//                 return (
+//                     <div key={index} ref={ref} className="vertical-timeline-element">
+                
+//                         <VerticalTimelineElement 
+//                             visible={inView}
+//                             // animate={true}
+//                             contentStyle={
+//                                 theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+//                                 //   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+//                                 boxShadow: "none",
+//                                 border: "1px solid rgba(0, 0, 0, 0.05)",
+//                                 textAlign: "left",
+//                                 padding: "1.3rem 2rem",
+//                             }}
+//                             contentArrowStyle={{
+//                                 borderRight: "0.4rem solid #9ca3af",
+//                                 //   theme === "light"
+//                                 //     ? "0.4rem solid #9ca3af"
+//                                 //     : "0.4rem solid rgba(255, 255, 255, 0.5)",
+//                             }}
+//                             date={item.date}
+//                             icon={item.icon}
+//                             iconStyle={{
+//                                 background: "white",
+//                                 //   theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+//                                 fontSize: "1.5rem",
+//                             }}
+
+//                             >
+//                                 <h3 className="font-semibold capitalize">{item.title}</h3>
+//                                 <p className="font-normal !mt-0">{item.location}</p>
+//                                 <p className="!mt-1 !font-normal text-gray-700">{item.description}</p>
+//                         </VerticalTimelineElement>
+                     
+//                     </div>
+//                     );
+//                 })
+
+//             }
+//         </VerticalTimeline>
+//     </section>
+// )
