8b91fd31f9b9ba7f11c112b3ae1085a5b3b11adb
chadjowens <chadjowens@gmail.com>
2024-04-01 11:33:20 -0500

    Updated "sendEmail" to pass data to  the "contact-form-email" and added styling (using React Resend & Resend Email).
    Updated submit button to show pending state.

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,10 @@
 "use server";
 
 // import { send } from "process";
+import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -32,18 +34,26 @@ export const sendEmail = async (formData: FormData) => {
             error: "Invalid message or email",
         };
     }
-
+    let data;
     try {
-        await resend.emails.send({
+        data = await resend.emails.send({
             from: 'Contact Form <onboarding@resend.dev>',
             to: 'chadjowens@gmail.com',
-            subject: 'New message from portfolio',
+            subject: 'Message from contact form',
             reply_to: senderEmail as string,
-            text: message as string,
+            // text: message as string,
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string, 
+            }),
+            // react: <ContactFormEmail message={message} senderEmail={senderEmail} /> 
         });
     }   catch (error: unknown) {
         return {
             error: getErrorMessage(error),
-        }
+        };
     }
+    return {
+        data,
+    };
 };
