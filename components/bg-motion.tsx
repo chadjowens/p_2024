50cbb13a0556db62f53c2ac79b24d30badb81b25
chadjowens <chadjowens@gmail.com>
2024-04-03 10:19:22 -0500

    Update to color background.

--- components/bg-motion.tsx
+++ components/bg-motion.tsx
@@ -23,11 +23,15 @@ const BackgroundMotion = () => {
 
   return (
     <>
-        <motion.div className="bg-[#efd5d6] fixed top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"
+        <motion.div className="bg-[#efd5d6] fixed top-[2rem] -z-10 right-[11rem] h-[50rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[20rem] 2xl:left-[5rem dark:bg-[#946263]"
         style={{ y: y1 }}
         ></motion.div>
 
-        <motion.div className="bg-[#d2cef2] fixed top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
+        <motion.div className="bg-[#d2cef2] fixed top-[12rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
+        style={{ y: y2 }}
+        ></motion.div>
+
+        <motion.div className="bg-[#ebf2ce] fixed top-[0rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[33rem] lg:left-[40rem] xl:left-[40rem] 2xl:left-[40rem] dark:bg-[#676394]"
         style={{ y: y2 }}
         ></motion.div>
     </>
